de20e5330b565406ea351d72f92b6b467d132cfb
Emily <easchoettler@gmail.com>
2023-02-27 15:34:26 -0500

    Add syle & refresh grid features

--- src/grid.js
+++ src/grid.js
@@ -1,19 +1,29 @@
 /* eslint-disable import/prefer-default-export */
 
-function createGrid(container, objList) {
+export function displayGrid(container, player, gameboard) {
+    const list = gameboard.objList;
     for (let i=0; i < 100; i+= 1) {
         const square = document.createElement("div");
         square.classList.add("square");
-        if (objList[i].ship != null) {
+        if (player.activeTurn === false) square.classList.add("active");
+        if (list[i].ship != null && list[i].attacked === null) {
             square.classList.add("ship");
+        } else if (list[i].ship != null && list[i].attacked === true) {
+            square.classList.add("hit-ship");
+        } else if (list[i].ship === null && list[i].attacked === true) {
+            square.classList.add("hit-miss");
         }
         container.appendChild(square);
     }
 }
 
-export function createGrids(objList1, objList2) {
-    const p1Container = document.querySelector(".p1-container");
-    const p2Container = document.querySelector(".p2-container");
-    createGrid(p1Container, objList1);
-    createGrid(p2Container, objList2);
+function removeGrid(container) {
+    while (container.firstChild) {
+        container.removeChild(container.firstChild);
+    }
+}
+
+export function refreshGrid(container, player, gameboard) {
+    removeGrid(container);
+    displayGrid(container, player, gameboard);
 }

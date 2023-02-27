9530f363867b313fbbfaade72ef37f38fbdfe299
Emily <easchoettler@gmail.com>
2023-02-27 16:19:59 -0500

    Add event listeners to board squares

--- src/grid.js
+++ src/grid.js
@@ -5,7 +5,7 @@ export function displayGrid(container, player, gameboard) {
     for (let i=0; i < 100; i+= 1) {
         const square = document.createElement("div");
         square.classList.add("square");
-        if (player.activeTurn === false) square.classList.add("active");
+        
         if (list[i].ship != null && list[i].attacked === null) {
             square.classList.add("ship");
         } else if (list[i].ship != null && list[i].attacked === true) {
@@ -13,6 +13,17 @@ export function displayGrid(container, player, gameboard) {
         } else if (list[i].ship === null && list[i].attacked === true) {
             square.classList.add("hit-miss");
         }
+
+        if (player.activeTurn === false) {
+            square.classList.add("active");
+            square.addEventListener("click", () => {
+                gameboard.receiveAttack(i);
+                refreshGrid(container, player, gameboard);
+            })
+        } else {
+            square.classList.remove("active");
+        }
+
         container.appendChild(square);
     }
 }

cdfecc62aa0ebdba90ae921a0c47725cd588b902
Emily <easchoettler@gmail.com>
2023-03-02 09:47:32 -0500

    Update square classes so computer board is hidden

--- src/grid.js
+++ src/grid.js
@@ -9,23 +9,22 @@ export const Grid = (container, player, gameboard) => {
         const square = document.createElement("div");
         square.classList.add("square");
         
-        // Add appropriate square classes
-        if (list[i].ship != null && list[i].attacked === null) {
-            square.classList.add("ship");
-        } else if (list[i].ship != null && list[i].attacked === true) {
-            square.classList.add("hit-ship");
-        } else if (list[i].ship === null && list[i].attacked === true) {
-            square.classList.add("hit-miss");
-        }
-
-        // Add/remove active & inactive classes
-        if (player.activeTurn === false) {
-            square.classList.remove("inactive");
-            square.classList.add("active");
-        } else {
-            square.classList.remove("active");
-            square.classList.add("inactive");
-        }
+        // Add appropriate square classes for human board
+        if (player.activeTurn === true) { 
+            if (list[i].ship != null && list[i].attacked === null) {
+                square.classList.add("ship");
+            } else if (list[i].ship != null && list[i].attacked === true) {
+                square.classList.add("hit-ship");
+            } else if (list[i].ship === null && list[i].attacked === true) {
+                square.classList.add("hit-miss");
+            }} else {
+                square.classList.add("active");
+                if (list[i].ship != null && list[i].attacked === true) {
+                    square.classList.add("hit-ship");
+                } else if (list[i].ship === null && list[i].attacked === true) {
+                    square.classList.add("hit-miss");
+                }
+            }
 
         container.appendChild(square);
     }}
